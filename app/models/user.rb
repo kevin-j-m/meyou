6b9495b962c2258ba2747d60a5795bb0f82f094e
Kevin Murphy <kmurphy@athenahealth.com>
2014-04-26 14:12:44 -0500

    User has a notion of statuses and keeps track of where they are in the registration process

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ActiveRecord::Base
+  validates_presence_of :first_name, :if => lambda {|user| user.sign_up_status == 'who_you_are'}
+  validates_presence_of :company, :if => lambda {|user| user.sign_up_status == 'about_you'}
+
+  def initialize(status = nil, *params)
+    @status = sign_up_status(status)
+    super(*params)
+  end
+
+  def sign_up_status(status = nil)
+    @status || status || sign_up_statuses.first
+  end
+
+  def next_sign_up_status
+    @status = sign_up_statuses[sign_up_statuses.index(@status)+1]
+  end
+
+  def sign_up_finished?
+    @status == sign_up_statuses.last
+  end
+
+  def sign_up_statuses
+    ['who_you_are', 'about_you']
+  end
 end
