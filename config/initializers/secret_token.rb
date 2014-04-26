e8a6e10f1374cade5a7c1f7d72e7a9ca7255f69b
Kevin Murphy <kmurphy@athenahealth.com>
2014-04-25 20:00:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meyou::Application.config.secret_key_base = '754dc38446aa483351d838f14dae058ffe07224bec9815daf09aec3f3261b4b1a9e4f1e9e6f54ecec087949e1a1f37b4e790cfaded715a40b5ad0ddda1482c02'
