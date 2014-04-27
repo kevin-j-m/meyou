4eddcc0679d5d2be818dedd1c47beaba946f1c49
Kevin Murphy <kmurphy@athenahealth.com>
2014-04-27 16:00:37 -0400

    Added user id to the reply model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427195452) do
+ActiveRecord::Schema.define(version: 20140427195809) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140427195452) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "replies", ["post_id"], name: "index_replies_on_post_id"
